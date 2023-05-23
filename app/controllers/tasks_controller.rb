7dffc34905236b4b1225326f0d6f8a994de79593
bfazz <brunofazz80@gmail.com>
2023-05-23 16:27:05 -0300

    5 - As a user, I can add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,16 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
   private
 
   def set_task
