6f2bfc5b4b38f799b819fb9e20ebfb7df2800d08
bfazz <brunofazz80@gmail.com>
2023-05-23 16:08:11 -0300

    4 - As a user, I can view the details of a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,23 @@
 class TasksController < ApplicationController
+  before_action :set_task, except: [:index, :new, :create]
+
   def index
     @tasks = Task.all
   end
+
+  def show
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  # STRONG_PARAMS
+
+  def task_params
+
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
